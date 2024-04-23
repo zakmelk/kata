4ab7429219a26b8fd5552eb6e2782a368a36ff0e
zakariamelk <melkbox@gmail.com>
2024-04-23 16:10:11 +0100

    Add unit tests for SingleThreadedEventBus

---
+++ src/test/java/com/hsbc/eventbus/SingleThreadedEventBusTest.java
@@ -0,0 +1,35 @@
+package com.hsbc.eventbus;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class SingleThreadedEventBusTest {
+    private static final SingleThreadedEventBus SINGLE_THREADED_EVENT_BUS = new SingleThreadedEventBus();
+
+
+    @Test
+    void publishEvent() {
+        // Given
+        SubscriberImpl subscriber1 = new SubscriberImpl("Subscriber 1");
+        SubscriberImpl subscriber2 = new SubscriberImpl("Subscriber 2");
+        SubscriberWithFilterImpl subscriberWithFilter1 = new SubscriberWithFilterImpl("Subscriber with filter 1");
+        SubscriberWithFilterImpl subscriberWithFilter2 = new SubscriberWithFilterImpl("Subscriber with filter 2");
+        SINGLE_THREADED_EVENT_BUS.addSubscriber(Integer.class,subscriber1);
+        SINGLE_THREADED_EVENT_BUS.addSubscriber(Integer.class,subscriber2);
+        SINGLE_THREADED_EVENT_BUS.addSubscriberForFilteredEvents(String.class,subscriberWithFilter1);
+        SINGLE_THREADED_EVENT_BUS.addSubscriberForFilteredEvents(String.class,subscriberWithFilter2);
+        SINGLE_THREADED_EVENT_BUS.addSubscriber(Integer.class,subscriberWithFilter2);
+        // When
+        SINGLE_THREADED_EVENT_BUS.publishEvent(new String("Event 1"));
+        SINGLE_THREADED_EVENT_BUS.publishEvent(new String("Event 2"));
+        SINGLE_THREADED_EVENT_BUS.publishEvent(Integer.valueOf(1));
+        SINGLE_THREADED_EVENT_BUS.publishEvent(Integer.valueOf(2));
+        SINGLE_THREADED_EVENT_BUS.publishEvent(Float.valueOf(2));
+        // Then
+        assertEquals(2,subscriber1.getReceivedObjects().size());
+        assertEquals(2,subscriber2.getReceivedObjects().size());
+        assertEquals(2,subscriberWithFilter1.getReceivedObjects().size());
+        assertEquals(4,subscriberWithFilter2.getReceivedObjects().size());
+    }
+}
---
+++ src/test/java/com/hsbc/eventbus/SubscriberWithFilterImpl.java
@@ -0,0 +1,28 @@
+package com.hsbc.eventbus;
+
+import lombok.Getter;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Getter
+public class SubscriberWithFilterImpl implements SubscriberWithFilter {
+    private final String name;
+    private final List<Object> receivedObjects;
+
+    public SubscriberWithFilterImpl(String name) {
+        this.name = name;
+        receivedObjects = new ArrayList<>();
+    }
+
+    @Override
+    public void update(Object o) {
+        System.out.println(String.format("Subscriber %s has received an event %s", name,o.getClass()));
+        receivedObjects.add(o);
+    }
+
+    @Override
+    public boolean isValid(Object event) {
+        return event instanceof String;
+    }
+}
