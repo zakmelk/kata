feb069c1021a1d98b1898e5ade78753da61b9b93
zakariamelk <melkbox@gmail.com>
2024-04-23 16:10:11 +0100

    Add unit tests for SingleThreadedEventBus

---
+++ src/test/java/com/hsbc/eventbus/SubscriberImpl.java
@@ -0,0 +1,23 @@
+package com.hsbc.eventbus;
+
+import lombok.Getter;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Getter
+public class SubscriberImpl implements Subscriber {
+    private final String name;
+    private final List<Object> receivedObjects;
+
+    public SubscriberImpl(String name) {
+        this.name = name;
+        receivedObjects = new ArrayList<>();
+    }
+
+    @Override
+    public void update(Object o) {
+        System.out.println(String.format("Subscriber %s has received an event %s", this.name,o.getClass()));
+        receivedObjects.add(o);
+    }
+}
