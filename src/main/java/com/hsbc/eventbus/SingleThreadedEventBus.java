36c3c50678c0d333cf12cd6be5485569df1c4a46
zakariamelk <melkbox@gmail.com>
2024-04-23 14:47:00 +0100

    Single threaded event bus

---
+++ src/main/java/com/hsbc/eventbus/SingleThreadedEventBus.java
@@ -0,0 +1,42 @@
+package com.hsbc.eventbus;
+
+import java.util.*;
+
+public class SingleThreadedEventBus implements EventBus{
+    private final Map<Class<?>, Set<Subscriber>> subscribersWithoutFilter = new HashMap<>();
+    private final Map<Class<?>, Set<SubscriberWithFilter>> subscribersWithFilter = new HashMap<>();
+
+    @Override
+    public void publishEvent(Object event) {
+        if(Objects.nonNull(event)){
+            publishEventWithoutFilter(event);
+            publishEventWithFilter(event);
+        }
+    }
+
+    private void publishEventWithoutFilter(Object o) {
+        Set<Subscriber> subscribers = subscribersWithoutFilter.getOrDefault(o.getClass(), new HashSet<>());
+        for(Subscriber subscriber : subscribers){
+            subscriber.update(o);
+        }
+    }
+
+    private void publishEventWithFilter(Object event) {
+        Set<SubscriberWithFilter> subscribers = subscribersWithFilter.getOrDefault(event.getClass(), new HashSet<>());
+        for(SubscriberWithFilter subscriber : subscribers){
+            if(subscriber.isValid(event)){
+                subscriber.update(event);
+            }
+        }
+    }
+
+    @Override
+    public void addSubscriber(Class<?> eventType, Subscriber subscriber) {
+        subscribersWithoutFilter.computeIfAbsent(eventType, key->new HashSet<>()).add(subscriber);
+    }
+
+    @Override
+    public void addSubscriberForFilteredEvents(Class<?> eventType, SubscriberWithFilter subscriber) {
+        subscribersWithFilter.computeIfAbsent(eventType,key->new HashSet<>()).add(subscriber);
+    }
+}
