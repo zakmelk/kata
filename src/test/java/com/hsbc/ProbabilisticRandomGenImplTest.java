46dddc52277bea401c279ba958ce89b40c1b8afe
zakariamelk <melkbox@gmail.com>
2024-04-23 13:19:59 +0100

    add unit tests for ProbabilisticRandomGenImpl

---
+++ src/test/java/com/hsbc/ProbabilisticRandomGenImplTest.java
@@ -0,0 +1,27 @@
+package com.hsbc;
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ProbabilisticRandomGenImplTest {
+    private static ProbabilisticRandomGen probabilisticRandomGen;
+
+    @BeforeAll
+    public static void setup(){
+        ProbabilisticRandomGen.NumAndProbability numAndProbability1 = new ProbabilisticRandomGen.NumAndProbability(1,0.2f);
+        ProbabilisticRandomGen.NumAndProbability numAndProbability2 = new ProbabilisticRandomGen.NumAndProbability(2,0.3f);
+        ProbabilisticRandomGen.NumAndProbability numAndProbability3 = new ProbabilisticRandomGen.NumAndProbability(3,0.1f);
+        ProbabilisticRandomGen.NumAndProbability numAndProbability4 = new ProbabilisticRandomGen.NumAndProbability(4,0.2f);
+        List<ProbabilisticRandomGen.NumAndProbability> pDistribution = List.of(numAndProbability1,numAndProbability2,numAndProbability3,numAndProbability4);
+        probabilisticRandomGen = new ProbabilisticRandomGenImpl(pDistribution);
+    }
+
+    @Test
+    void nextFromSample() {
+        assertTrue(probabilisticRandomGen.nextFromSample()>-1);
+    }
+}
