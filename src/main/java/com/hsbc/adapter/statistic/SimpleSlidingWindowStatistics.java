a2d6be6ea7e4cd5b51515ece1da19fa7d094d4ae
zakariamelk <melkbox@gmail.com>
2024-04-25 22:41:06 +0200

    Add unit tests for SimpleSlidingWindowStatistics

--- src/main/java/com/hsbc/adapter/statistic/SimpleSlidingWindowStatistics.java
+++ src/main/java/com/hsbc/adapter/statistic/SimpleSlidingWindowStatistics.java
@@ -6,10 +6,13 @@ import com.hsbc.domain.model.event.Statistics;
 import com.hsbc.domain.model.event.SubscriberWithFilter;
 import com.hsbc.domain.port.event.SlidingWindowStatistics;
 
+import java.util.Collections;
+import java.util.Queue;
 import java.util.concurrent.BlockingQueue;
 import java.util.concurrent.LinkedBlockingQueue;
 
 public class SimpleSlidingWindowStatistics implements SlidingWindowStatistics {
+
     private final BlockingQueue<Integer> measurements;
     private final StatisticsThrottler throttler;
 
@@ -29,12 +32,16 @@ public class SimpleSlidingWindowStatistics implements SlidingWindowStatistics {
 
     @Override
     public void subscribeForStatistics(SubscriberWithFilter subscriber) {
-        throttler.addSubscriberForFilteredEvents(Statistics.class, subscriber);
+        throttler.addSubscriberForFilteredEvents(StatisticsImpl.class, subscriber);
     }
 
     @Override
     public Statistics getLatestStatistics() {
         return new StatisticsImpl(measurements);
+    }
+
+    public BlockingQueue<Integer> getMeasurements() {
+        return measurements;
     }
 
 }
